767de48596f778fe04b84d15fb5f2f2b7d134191
phammaidung <phammaidung8@gmail.com>
2024-01-31 17:46:46 +0700

    completed counter demo

---
+++ flutter_counter_app/lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_counter_app/counter/counter.dart';
+
+class CounterApp extends MaterialApp {
+  const CounterApp({super.key}) : super(home: const CounterPage());
+}
